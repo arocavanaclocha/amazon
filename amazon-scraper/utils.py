86992d9f57cd8cb65d9e5a2a05d91855073dbfd8
Alberto <75672168+arocavanaclocha@users.noreply.github.com>
2020-12-12 16:36:35 +0100

    Create utils.py

---
+++ amazon-scraper/utils.py
@@ -0,0 +1,16 @@
+import json
+
+def give_format(file_name):
+  results = []
+  with open(file_name, 'r') as json_file:
+    for line in json_file:
+      results.append(line)
+
+
+  with open(file_name, 'w+') as json_file:
+    json_file.write("[")
+    for i in range(len(results)):
+      json_file.write(results[i])
+      if(i<len(results)-1):  
+        json_file.write(",")
+    json_file.write("]")
